866146ce3ffd5809938e11a4853d9ac5786df5b8
UmeshDhakal22 <dhakalumesh22@gmail.com>
2025-06-16 14:25:27 +0545

    Update preprocessing process

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv('ner_test.csv')
+df = pd.read_csv('baato_requests_places.csv')
+df = df[['name']]
+df = df.rename(columns={'name': 'Places'})
 df['Places'] = df['Places'].str.split(',').str[0]
 df = df.drop_duplicates()
 df.to_csv('ner_test.csv', index=False)
